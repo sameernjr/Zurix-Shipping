fe89f816af8c2546f575072a56f004a6dc0d7879
Sameer <njorogesameer@gmail.com>
2024-12-07 13:00:02 +0300

    Add template filesn and static file in the settings.py

--- zurix/zurix/settings.py
+++ zurix/zurix/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'zurix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT =[
+    BASE_DIR / 'staticfiles'
+]
+STATICFILES_DIRS =[
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
