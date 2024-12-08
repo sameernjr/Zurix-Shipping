c694a175b8ae8c12c013612eceb9d7d6d399e952
Sameer <njorogesameer@gmail.com>
2024-12-08 16:30:11 +0300

    Commented out the Static Root Function in settings and everything works now.

--- zurix/zurix/settings.py
+++ zurix/zurix/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT =[
-    BASE_DIR / 'staticfiles'
-]
+# STATIC_ROOT =[
+#     BASE_DIR / 'staticfiles'
+# ]
 STATICFILES_DIRS =[
     BASE_DIR / 'static'
 ]
