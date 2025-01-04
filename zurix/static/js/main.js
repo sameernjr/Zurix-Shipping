5d771d9d9d7bca8b7c54f611c50f6766eb24b0d3
Sameer <njorogesameer@gmail.com>
2025-01-05 00:21:47 +0300

    will finish FAQ section

--- zurix/static/js/main.js
+++ zurix/static/js/main.js
@@ -1,7 +1,18 @@
-const faqs = document.querySelectorAll('.faq');
+document.addEventListener('DOMContentLoaded', () => {
+    const faqs = document.querySelectorAll('.faq');
 
-faqs.forEach(faq => {
-    faq.addEventListener('click', () => {
-        faq.classList.toggle('active');
+    faqs.forEach(faq => {
+        faq.addEventListener('click', () => {
+            // Close all other Faqs
+            faqs.forEach(item => {
+                if (item !== faq && item.classList.contains('active')) {
+                    item.classList.remove('active');
+                }
+            });
+
+            // Toogle the Clicked FAQ
+
+            faq.classList.toggle('active');
+        });
     });
 });
