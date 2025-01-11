0dc69ded48c06c533fde30e1cb8a36f8a5e184b4
Sameer <njorogesameer@gmail.com>
2025-01-11 14:06:54 +0300

    adding static changes in settings

--- zurix/zurix/settings.py
+++ zurix/zurix/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # STATIC_ROOT =[
-#     BASE_DIR / 'staticfiles'
-# ]
+#      BASE_DIR / 'staticfiles'
+#  ]
 STATICFILES_DIRS =[
     BASE_DIR / 'static'
 ]
