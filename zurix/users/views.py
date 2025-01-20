d63903b5df6da2ae1e30bb5fb0e98cd80b4abf71
Sameer <njorogesameer@gmail.com>
2025-01-20 22:33:30 +0300

    signup function in views.py in user

--- zurix/users/views.py
+++ zurix/users/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully! Please log in.')
+            return redirect ('login')
+        
+    else:
+        form = SignUpForm()
+    return render (request, 'users/signup.html', {'form':form})
