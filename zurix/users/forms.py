0c41cb56e42d2e190d61da6f75477bf779d5d48e
Sameer <njorogesameer@gmail.com>
2025-01-20 22:22:42 +0300

    Add SignUpForm Class in forms.py in users

---
+++ zurix/users/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+    phone_number = forms.ChoiceField(max_length=15)
+    address = forms.CharField(widget=forms.Textarea)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email','phone_number','address','password1','password2']
