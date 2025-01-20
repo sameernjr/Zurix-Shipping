b571bccb0d50075d95eb1b3c84e35c930e6969e4
Sameer <njorogesameer@gmail.com>
2025-01-20 22:42:42 +0300

    Add urls paths in users

--- zurix/users/urls.py
+++ zurix/users/urls.py
@@ -2,3 +2,10 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .import views
 
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('profile/', views.profile, name='profile'),
+]
