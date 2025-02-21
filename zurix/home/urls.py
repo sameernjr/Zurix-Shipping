627a5ff4afefba057b6b24fb2b01351b35ead1d2
Sameer <njorogesameer@gmail.com>
2025-02-21 23:32:42 +0300

    add name to home page in home.urls

--- zurix/home/urls.py
+++ zurix/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view),
+    path('', views.index_view, name='home'),
     path('About/', views.about_view),
     path('Contact/', views.contact_view, name='contact'),
 ]
