845d76675173131a94a3beaff0483ea241acdce1
Sameer <njorogesameer@gmail.com>
2025-02-21 23:28:08 +0300

    removed an extra logout path in users.urls

--- zurix/users/urls.py
+++ zurix/users/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
