3266a8642d6c29fb6c60abe942ed6e3337307cee
Sameer <njorogesameer@gmail.com>
2025-02-21 23:52:20 +0300

    added names for all urls in home.urls

--- zurix/home/urls.py
+++ zurix/home/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='home'),
-    path('About/', views.about_view),
+    path('About/', views.about_view , name='about'),
     path('Contact/', views.contact_view, name='contact'),
 ]
 
