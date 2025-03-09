3980e4cdc5f0bd57c9d632dc60416ead13796087
Sameer <njorogesameer@gmail.com>
2025-03-09 18:55:56 +0300

    Registered shipping to admin

--- zurix/shipping/admin.py
+++ zurix/shipping/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ShippingOrder
 
 # Register your models here.
+
+@admin.register(ShippingOrder)
+class ShippingOrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'item_name', 'cost', 'created_at')
