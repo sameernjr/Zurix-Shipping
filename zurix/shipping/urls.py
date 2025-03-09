758ed3d8fd84cfc869ad174d2d7b1cc4462b1bbf
Sameer <njorogesameer@gmail.com>
2025-03-09 18:50:18 +0300

    Created  URls for Shipping

---
+++ zurix/shipping/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('form/', views.shipping_form, name='shipping_form'),
+    path('receipt/<int:order_id>/', views.generate_receipt, name='generate_receipt'),
+    path('history/', views.user_history, name='user_history'),
+    path('admin/dashboard/', views.user_history, name='user_history'),
+]
