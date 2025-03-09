5db5ed3d370053da6686b359afebb9aa5cf6147a
Sameer <njorogesameer@gmail.com>
2025-03-09 12:10:12 +0300

    Created ShippingOrder Model

--- zurix/shipping/models.py
+++ zurix/shipping/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class ShipingOrder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    item_name = models.CharField(max_length=100)
+    weight = models.FloatField()
+    dimensions = models.CharField(max_length=50)
+    shipping_address = models.TextField()
+    cost = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #(self.id) by {self.user.username}"
