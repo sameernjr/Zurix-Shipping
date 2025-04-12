6d9e2e7156f06163e477bf39a93aa3b4b586984b
Sameer <njorogesameer@gmail.com>
2025-04-12 13:22:02 +0300

    created package information in models

--- zurix/shipping/models.py
+++ zurix/shipping/models.py
@@ -19,3 +19,13 @@ class ShippingOrder(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    #Package Information
+    package_weight = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinLengthValidator(0.1)],
+        help_text="Weight in kg",
+    )
+
+    
