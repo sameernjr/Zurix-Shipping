a9e0021b94d429c13ee12acb4b904f8513f73e2b
Sameer <njorogesameer@gmail.com>
2025-04-12 13:17:22 +0300

    Created models for dropdown menus

--- zurix/shipping/models.py
+++ zurix/shipping/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+class ShippingOrder(models.Model):
+    REGION_CHOICES = [
+        ('Malaysia,','Malaysia'),
+        ('Asia','Asia'),
+        ('International','International'),
+    ]
+
+    SHIPPING_METHOD_CHOICES = [
+        ('Standard','Standard'),
+        ('Express','Express'),
+    ]
