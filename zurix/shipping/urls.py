8a917b187e92d41eaa2ec785f1ccc7bc2fcb63fc
Sameer <njorogesameer@gmail.com>
2025-05-16 19:01:07 +0300

    deleted shipping app

--- zurix/shipping/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from shipping.views import shipping_request, order_success, order_history
-
-urlpatterns = [
-    path(' ', shipping_request, name='shipping_request'),
-    path('order/success/<int:order_id>/', order_success, name='order_success'),
-    path('orders/', order_history, name='order_history'),
-]
